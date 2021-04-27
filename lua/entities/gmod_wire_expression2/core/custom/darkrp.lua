938b50b1e4467979a6cf574ef6dcdd37af68a2bc
Smiggy <itsespresso@gmail.com>
2021-04-27 20:11:25 +0100

    Update e2darkrpfuncs

--- lua/entities/gmod_wire_expression2/core/custom/darkrp.lua
+++ lua/entities/gmod_wire_expression2/core/custom/darkrp.lua
@@ -1,19 +1,21 @@
-E2Lib.RegisterExtension("darkrp", true)
+E2Lib.RegisterExtension("DarkRP Functions", true, "Adds various useful functions for use in the DarkRP gamemode")
 
--- Money printers (!!!WARLOCK!!!)
+printerClass = "sent_elegant_printer" -- change to your servers printers class ( some functions below will not work if you're not using elegant printers, so just remove them :D )
+
+-- Printers
 e2function number entity:isPrinter()
 	if not IsValid(this) then  return 0 end
-	if string.Right(this:GetClass(), 13) == "money_printer" then 
+	if this:GetClass() == printerClass then
 		return 1
 	else
 		return 0
 	end
 end
 
-e2function number entity:printedMoney()
+e2function number entity:printerAmount()
 	if not IsValid(this) then  return -1 end
-	if string.Right(this:GetClass(), 13) == "money_printer" then 
-		return this:GetNWInt("PrintA")
+	if this:GetClass() == printerClass then
+		return this:GetMoney()
 	else
 		return -1
 	end
@@ -21,10 +23,56 @@ end
 
 e2function string entity:printerType()
 	if not IsValid(this) then return "" end
-	if string.Right(this:GetClass(), 13) == "money_printer" then 
+	if this:GetClass() == printerClass then
 		return this:GetClass()
 	else
 		return ""
+	end
+end
+
+e2function entity entity:printerOwner()
+	if not IsValid(this) then return end
+	if this:GetClass() == printerClass then
+		return this:Getowning_ent()
+	end
+end
+
+e2function string entity:printerTier()
+	if not IsValid(this) then return end
+	if this:GetClass() == printerClass then
+		return this:GetTier()
+	end
+end
+
+e2function number entity:printerInkAmount()
+	if not IsValid(this) then return end
+	if this:GetClass() == printerClass then
+		return this:GetInk()
+	end
+end
+
+e2function number entity:printerInkMax()
+	if not IsValid(this) then return end
+	if this:GetClass() == printerClass then
+		return this:GetMaxInk()
+	end
+end
+
+e2function number entity:printerHealth()
+	if not IsValid(this) then return end
+	if this:GetClass() == printerClass then
+		return this:GetDurability()
+	end
+end
+
+e2function number entity:printerHQCooling()
+	if not IsValid(this) then return end
+	if this:GetClass() == printerClass then
+		if this:GetHQCooling() == 1 then
+			return 1
+		else
+			return 0
+		end
 	end
 end
 
